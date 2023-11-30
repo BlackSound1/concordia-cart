e1670872a2d67717249a78a9c879d9c4cd9c3661
BlackSound1 <matthewr@videotron.ca>
2023-11-30 18:48:52 -0500

    changes to pincode test

--- src/tests/unit/RegisterWithoutPin.java
+++ src/tests/unit/RegisterWithoutPin.java
@@ -14,19 +14,16 @@ import org.openqa.selenium.support.ui.Select;
 import org.junit.Test;
 
 
-
-
 public class RegisterWithoutPin {
 	//declare web driver
 	static WebDriver driver;
 	
 	@BeforeClass
 	public static void initDriver() {
-		
 		driver = new FirefoxDriver();
-		
 	}
 	
+	@Test
 	public void register() {
 		//go to registration page
 		driver.get("http://localhost:8080/shopping-cart/register.jsp");
@@ -34,19 +31,21 @@ public class RegisterWithoutPin {
 		//declare form elements 
 		WebElement username = driver.findElement(By.name("username"));
 		WebElement email = driver.findElement(By.name("email"));
-		WebElement last = driver.findElement(By.name("last_name"));
+		// WebElement last = driver.findElement(By.name("last_name"));
 		WebElement mobile = driver.findElement(By.name("mobile"));
 		WebElement password = driver.findElement(By.name("password"));
 		WebElement confirm = driver.findElement(By.name("confirmPassword"));
-		WebElement submit = driver.findElement(By.name("submit"));
+		WebElement address = driver.findElement(By.name("address"));
+		WebElement submit = driver.findElement(By.id("submit"));
 		
 		//filling inputs 
 		username.sendKeys("guest");
-		last.sendKeys("test");
+		//last.sendKeys("test");
 		email.sendKeys("guest_test@gmail.com");
 		password.sendKeys("guest1");
 		confirm.sendKeys("guest1");
-		mobile.sendKeys("123-456-789");
+		mobile.sendKeys("123456789");
+		address.sendKeys("123");
 		submit.click();
 		
 		/**tests if after inputting and submitting the modified registration
@@ -55,4 +54,10 @@ public class RegisterWithoutPin {
 		
 		assertEquals(driver.getCurrentUrl(),"http://localhost:8080/shopping-cart/RegisterSrv");
 
-	}}
+	}
+
+	@AfterClass
+	public static void closeDriver() {
+		driver.quit();
+	}
+}
